513bcb4c80873fca09ca1fc9dde915400abc7ac8
Piirtola <rassepiirtola@gmail.com>
2023-08-04 04:47:41 +0300

    feat(b): Add RDBConnectionWrapper + tests

---
+++ live_arbitrage_identificator/backend/db_management/rdb_connection_wrapper.py
@@ -0,0 +1,66 @@
+from psycopg2.pool import ThreadedConnectionPool
+from psycopg2 import Error
+import psycopg2.extensions
+
+from live_arbitrage_identificator.backend.db_management.db_config import DBConfig
+
+class RDBConnectionWrapper:
+    """
+    Wrapper class for managing connections to a relational database (RDB) using connection pooling.
+
+    This class leverages psycopg2's `ThreadedConnectionPool` to maintain a pool of connections to the database,
+    optimizing resource utilization and performance. The connection pool's size can be controlled by adjusting
+    the `minconn` and `maxconn` parameters.
+
+    The context management protocol is implemented, allowing the use of the `with` statement to manage
+    connections, ensuring proper acquisition and release of connections from the pool.
+
+    x = RDBConnectionWrapper(config)
+     -> Initializes the connection pool with the provided configuration parameters.
+
+    Params:
+        config (DBConfig): An instance of DBConfig containing the necessary configuration parameters
+            for connecting to the database, including database name, user, password, host, and port.
+
+    Attributes:
+        pool (ThreadedConnectionPool): The connection pool for managing connections to the database.
+        conn (Optional[psycopg2.extensions.connection]): The current active connection, if any.
+    """
+
+    def __init__(self, config: DBConfig):
+        self.pool = ThreadedConnectionPool(
+            minconn=1,
+            maxconn=10,
+            dbname=config.DB_NAME,
+            user=config.DB_USER,
+            password=config.DB_PASSWORD,
+            host=config.DB_HOST,
+            port=config.DB_PORT,
+        )
+        self.conn = None
+
+    def __enter__(self) -> psycopg2.extensions.connection:
+        """
+        Acquires a connection from the connection pool and returns it.
+
+        Raises:
+            ConnectionError: If there is a failure in establishing the connection to the database.
+
+        Returns:
+            psycopg2.extensions.connection: The acquired connection.
+        """
+        try:
+            self.conn = self.pool.getconn()
+            return self.conn
+        except Error as e:
+            raise ConnectionError(f"Failed to connect to the database: {str(e)}") from e
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """
+        Releases the acquired connection back to the connection pool.
+
+        This method ensures that connections are returned to the pool when no longer needed,
+        maintaining the efficiency and integrity of the pool.
+        """
+        self.pool.putconn(self.conn)
+        self.conn = None
---
+++ live_arbitrage_identificator/backend/tests/test_db_management/test_rdb_connection_wrapper.py
@@ -0,0 +1,37 @@
+import unittest
+from live_arbitrage_identificator.backend.db_management.db_config import DBConfig
+from live_arbitrage_identificator.utils.load_env import load_db_env
+from live_arbitrage_identificator.backend.db_management.rdb_connection_wrapper import RDBConnectionWrapper  # Make sure to import your RDBConnectionWrapper class
+import psycopg2.extensions
+
+class TestRDBConnectionWrapper(unittest.TestCase):
+    """
+    Simple test case for the RDBConnectionWrapper class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.db_config = DBConfig(**load_db_env())
+
+    def test_connection_acquisition(self):
+        """
+        Test that a connection can be successfully acquired from the pool.
+        """
+        with RDBConnectionWrapper(self.db_config) as conn:
+            self.assertIsNotNone(conn)
+            self.assertIsInstance(conn, psycopg2.extensions.connection)
+
+    def test_connection_release(self):
+        """
+        Test that a connection is released back to the pool after use.
+        """
+        wrapper = RDBConnectionWrapper(self.db_config)
+        with wrapper:
+            ... # acquire and release connection
+
+        # Check that the connection was released
+        self.assertIsNone(wrapper.conn)
+
+
+if __name__ == '__main__':
+    unittest.main()
