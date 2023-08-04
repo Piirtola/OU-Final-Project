b432efcac641cf821dfe06f1aa1ffb7ef81094e6
Piirtola <rassepiirtola@gmail.com>
2023-08-04 04:20:17 +0300

    feat(b): Add db_config.py

---
+++ live_arbitrage_identificator/backend/db_management/db_config.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+class DBConfig(BaseModel):
+    """
+    Represents the configuration for managing database connections.
+
+    Attributes:
+        DB_NAME (str): The name of the database.
+        DB_USER (str): The username to connect to the database.
+        DB_PASSWORD (str): The password to connect to the database.
+        DB_HOST (str): The hostname or IP address of the database server.
+        DB_PORT (str): The port number to use when connecting to the database server.
+    """
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: str
