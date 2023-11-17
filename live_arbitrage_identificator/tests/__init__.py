b6444f1e053745ec807b0848e482f731af7071b3
piirtola <rassepiirtola@gmail.com>
2023-11-17 16:16:19 +0200

    p

---
+++ live_arbitrage_identificator/tests/__init__.py
@@ -0,0 +1,8 @@
+def main():
+    """this function calculates the assets - liabilities = equity"""
+    assets = 8000
+    liabilities = 2000
+    equity = assets - liabilities
+    print(equity)
+
+main()
